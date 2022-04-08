7edf3cba68a7fcf5152997db60b22864f6777131
patrapritish1 <patrapritish1@gmail.com>
2022-04-08 18:19:19 +0530

    committng code

---
+++ src/test/java/MavenProject/MavenJava/RestAssuredAPITest.java
@@ -0,0 +1,21 @@
+package MavenProject.MavenJava;
+
+import org.testng.annotations.Test;
+
+
+
+public class RestAssuredAPITest{
+	
+@Test
+public void RestassuredAPImethod() {
+
+	System.out.println("RestAPI");
+}
+
+@Test
+public void RestAssuredAPITestdata() {
+
+	System.out.println("RestAPITestdata");
+}
+
+}
---
+++ src/test/java/MavenProject/MavenJava/SeleniumAPITest.java
@@ -0,0 +1,21 @@
+package MavenProject.MavenJava;
+
+import org.testng.annotations.Test;
+
+
+
+public class SeleniumAPITest{
+	
+@Test
+public void SeleniumAPImethod() {
+
+	System.out.println("SleniumAPI");
+}
+
+@Test
+public void Testdata() {
+
+	System.out.println("SleniumAPITestdata");
+}
+
+}
